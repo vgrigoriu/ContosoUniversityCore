7b799eff7b5d6500a23c2056230b6147e06d0b50
Victor Grigoriu <vgrigoriu@gmail.com>
2016-10-28 12:08:26 +0300

    Ignore members to make automapper test pass

--- src/ContosoUniversityCore/Features/Instructor/MappingProfile.cs
+++ src/ContosoUniversityCore/Features/Instructor/MappingProfile.cs
@@ -12,7 +12,9 @@
             CreateMap<Course, Index.Model.Course>();
             CreateMap<Enrollment, Index.Model.Enrollment>();
 
-            CreateMap<Instructor, CreateEdit.Command>();
+            CreateMap<Instructor, CreateEdit.Command>()
+                .ForMember(command => command.SelectedCourses, member => member.Ignore())
+                .ForMember(command => command.AssignedCourses, member => member.Ignore());
             CreateMap<CourseInstructor, CreateEdit.Command.CourseInstructor>();
 
             CreateMap<Instructor, Details.Model>();
